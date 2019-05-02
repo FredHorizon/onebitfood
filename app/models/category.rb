f2e55c3dc5fa6f74d5004a206853a1f30c621824
FredHorizon <fredoliveira.sys@gmail.com>
2019-05-02 22:17:50 +0000

    Implementa as validações

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
     has_many :restaurants # Uma categoria possui vários restaurantes
+    
+    validates :title, presence: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,13 @@ class Restaurant < ApplicationRecord
   has_many :product_categories
   has_many :orders
   has_many :reviews
+
+  validates_associated :category # é obrigatório haver uma categoria antes de inserir um restaurante
+  validates :name, presence: true
+  validates :status, presence: true
+  validates :delivery_tax, presence: true
+  validates :city, presence: true
+  validates :street, presence: true
+
+  enum status: { closed: 0, open: 1 } # um restaurante pode estar ABERTO ou FECHADO
 end
