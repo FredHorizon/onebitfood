a7914697056dc492685f547d7dbff84f686f7ec5
FredHorizon <fredoliveira.sys@gmail.com>
2019-05-04 21:28:44 +0000

    Configura os controllers para devolver os restaurantes e as categorias

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,28 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: :show
+
   def index
+    @restaurants = Restaurant.near(params[:city] || 'São Paulo') # 'near' método da geolocalização que fará uma busca próximo ao params city. Caso não exista o params city, ele irá procurar próximo a 'São Paulo'
+    filter_by_category if params[:category]
+    render json: @restaurants
   end
 
   def show
+    render json: @restaurant, product_categories: true
   end
 
   def search
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find_by(id: params[:id])
+  end
+
+  def filter_by_category
+    @restaurants = @restaurants.select do |r|
+      r.category.title == params[:category]
+    end
   end
 end
